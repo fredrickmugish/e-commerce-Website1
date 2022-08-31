2268f3c50f25e554cf5a3941c315b4e707101ab0
fredrickmugish <fredrickgotfried@gmail.com>
2022-08-31 16:09:44 -0400

    second commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        @vite('resources/js/app.js')
+                   
+    </head>
+    <body class="antialiased">
+      
+<div class="container h-8 mx-auto">
+    <div class="flex flex-row justify-between">
+    <div class="flex flex-row">
+<div class="bg-gradient-to-r from-purple-600 to-red-600 w-10 h-10 rounded-lg"></div>
+<h1 class="text-3xl text-gray-600 ml-2">Logo</h1>
+</div>
+<div>
+<a href="" class="">Home</a>
+</div>
+
+
+
+    </div>
+</div>
+    </body>
+</html>
